5dc28d6dc8122ea4e686fd1c831deaaea79dc89a
Jenish Pokhrel <susantp056@gmail.com>
2024-08-08 21:33:00 +0545

    User Profile Page

--- frontend/src/components/Profile/UserDetails.tsx
+++ frontend/src/components/Profile/UserDetails.tsx
@@ -5,31 +5,32 @@ const UserDetails = () => {
     const { user: currentUser } = useAuth()
 
     return (
-        <div className=" home w-fit border-x-4 border-y-4 ">
-            <div className="flex justify-center bg-slate-300 rounded-full h-28 w-28 py-10 items-center
-                relative top-20 border mx-auto">
-                <h2 className=" font-bold text-2xl">{currentUser?.firstName.charAt(0)}</h2>
+        <div className="w-[100%] p-16 ml-32 ">
+            <div>
+                <div className="flex justify-center bg-slate-300 rounded-full h-28 w-28 py-10 items-center
+                    border">
+                    <h2 className=" font-bold text-2xl">{currentUser?.firstName.charAt(0)}</h2>
+                </div>
             </div>
-            <div className=" w-[50%] relative top-28 left-28 border">
-                <div className=" m-2">
-                    <h2 className=" text-2xl font-light">UserName: {currentUser?.username}</h2>
+            <div className=" relative top-10">
+                <div className=" mb-5">
+                    <h2 className=" text-2xl font-bold">UserName </h2>
+                    <p className=" text-[20px]">{currentUser?.username}</p>
                 </div>
-                <div className=" flex justify-evenly">
-                    <div className=" mr-2">
-                        <h2 className=" text-2xl font-light">First Name: {currentUser?.firstName}</h2>
-                    </div>
-                    <div>
-                        <h2 className=" text-2xl font-light">Last Name: {currentUser?.lastName}</h2>
-                    </div>
+                <div className=" mb-5">
+                    <h2 className=" text-2xl font-bold">Name </h2>
+                    <p className=" text-[20px]">{currentUser?.firstName} {currentUser?.lastName}</p>
                 </div>
-                <div>
-                    <div>
-                        <h2>Email Address: {currentUser?.email}</h2>
-                    </div>
-                    <div>
-                        <h2>Address: {currentUser?.address ? currentUser.address : 'No Address Provided'}</h2>
-                    </div>
+                <div className=" mb-5">
+                    <h2 className=" text-2xl font-bold">Email Address </h2>
+                    <p className=" text-[20px]">{currentUser?.email}</p>
                 </div>
+                <div className=" mb-5">
+                    <h2 className=" text-2xl font-bold">Address</h2>
+                    <p className=" text-[20px]">{currentUser?.address ? currentUser.address : 'No Address Provided'}</p>
+                </div>
+                
+
             </div>
         </div>
     )
--- frontend/src/pages/main/Profile.tsx
+++ frontend/src/pages/main/Profile.tsx
@@ -11,51 +11,51 @@ import Libraries from "../../components/Profile/Libraries";
 
 const Profile = () => {
 
-    const { user: currentUser } = useAuth()
+  const { user: currentUser } = useAuth()
 
-    const [activeSection, setActiveSection] = useState('user')
+  const [activeSection, setActiveSection] = useState('user')
 
-    return (
-        <div>
-            <Navbar />
-            <div className="flex w-full">
-              <div className="w-[20%] bg-gray-100">
-                <div className=" flex justify-center bg-slate-300 rounded-full h-20 w-20 py-10 items-center relative top-16 border mx-auto">
-                    <h2 className=" font-bold text-2xl">{currentUser?.firstName.charAt(0)}</h2>
-                </div>
-                <div>
-                    <h1 className=" text-center px-10 mt-24 mb-7 text-3xl font-bold">Hello, {currentUser?.firstName}</h1>
-                </div>
-                <hr />
-                <div className="">
-                    <div className=" flex justify-center items-center w-full cursor-pointer hover:bg-gray-200 transition-all" 
-                      onClick={() => setActiveSection('user')}>
-                        <FaRegUser size={18} className=" mr-10"/>
-                        <h2 className=" text-1xl py-2 m-1">User</h2>
-                    </div>
-                    <hr />
-                    <div className=" flex justify-center items-center w-full cursor-pointer hover:bg-gray-200 transition-all"
-                      onClick={() => setActiveSection('blogs')}>
-                        <RiArticleFill size={18} className=' mr-5'/>
-                        <h2 className=" text-1xl py-2 ml-8 m-1">Blogs</h2>
-                    </div>
-                    <hr />
-                    <div className=" flex justify-center items-center w-full cursor-pointer hover:bg-gray-200 transition-all"
-                      onClick={() => setActiveSection('libraries')}>
-                        <IoLibrarySharp size={18} className=" mr-3"/>
-                        <h2 className=" text-1xl py-2 ml-5 m-1">Libraries</h2>
-                    </div>
-                </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="flex">
+        <div className="w-[20%] bg-gray-100">
+          <div className=" flex justify-center bg-slate-300 rounded-full h-20 w-20 py-10 items-center relative top-16 border mx-auto">
+            <h2 className=" font-bold text-2xl">{currentUser?.firstName.charAt(0)}</h2>
+          </div>
+          <div>
+            <h1 className=" text-center px-10 mt-24 mb-7 text-3xl font-bold">Hello, {currentUser?.firstName}</h1>
+          </div>
+          <hr />
+          <div className="">
+            <div className=" flex justify-center items-center w-full cursor-pointer hover:bg-gray-200 transition-all"
+              onClick={() => setActiveSection('user')}>
+              <FaRegUser size={18} className=" mr-10" />
+              <h2 className=" text-1xl py-2 m-1">User</h2>
             </div>
-            <div>
-              {activeSection === 'user' && <UserDetails/>}
-              {activeSection === 'blogs' && <Blogs/>}
-              {activeSection === 'libraries' && <Libraries/>}
+            <hr />
+            <div className=" flex justify-center items-center w-full cursor-pointer hover:bg-gray-200 transition-all"
+              onClick={() => setActiveSection('blogs')}>
+              <RiArticleFill size={18} className=' mr-5' />
+              <h2 className=" text-1xl py-2 ml-8 m-1">Blogs</h2>
             </div>
+            <hr />
+            <div className=" flex justify-center items-center w-full cursor-pointer hover:bg-gray-200 transition-all"
+              onClick={() => setActiveSection('libraries')}>
+              <IoLibrarySharp size={18} className=" mr-3" />
+              <h2 className=" text-1xl py-2 ml-5 m-1">Libraries</h2>
             </div>
-            <Footer/>
+          </div>
         </div>
-    )
+        <div>
+          {activeSection === 'user' && <UserDetails />}
+          {activeSection === 'blogs' && <Blogs />}
+          {activeSection === 'libraries' && <Libraries />}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default Profile
